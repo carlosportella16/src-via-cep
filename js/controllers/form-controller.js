7888213854c276cb8dd83271049a2d7b7607a4dd
Carlos Eduardo Rosa Portella <carlosportella16@hotmail.com>
2023-02-04 16:42:28 -0300

    handle input number change

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -3,7 +3,7 @@ import Address from '../models/address.js';
 function State() {
 
     this.address = new Address();
-    
+
     this.btnSave = null;
     this.btnClear = null;
 
@@ -31,5 +31,19 @@ export function init() {
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
 
-    console.log(state);
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+
+}
+
+function handleInputNumberChange(event) {
+    if (event.target.value == "") {
+        setFormError("number", "Campo requerido");
+    } else {
+        setFormError("number", "");
+    }
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
 }
