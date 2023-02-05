a30d1c90c42ce62046363f6d916fb5b8a203a948
Carlos Eduardo Rosa Portella <carlosportella16@hotmail.com>
2023-02-05 15:50:03 -0300

    Handle buttom Clear event click

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -33,6 +33,7 @@ export function init() {
 
     state.inputNumber.addEventListener('change', handleInputNumberChange);
 
+    state.btnClear.addEventListner('click', handleBtnClearClick);
 }
 
 function handleInputNumberChange(event) {
@@ -41,6 +42,23 @@ function handleInputNumberChange(event) {
     } else {
         setFormError("number", "");
     }
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm(){
+    state.inputCep.value = "";
+    state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.inputCep.focus;
 }
 
 function setFormError(key, value) {
