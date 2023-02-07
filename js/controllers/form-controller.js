49ecc231cff48ec7c159443ff9ea3df6b355847f
Carlos Eduardo Rosa Portella <carlosportella16@hotmail.com>
2023-02-06 22:33:38 -0300

    creating listController to add new cards

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,5 +1,6 @@
 import Address from '../models/address.js';
 import * as addressService from '../services/address-service.js';
+import * as listController from '../controllers/list-controller.js'
 
 function State() {
 
@@ -64,7 +65,7 @@ async function handleInputCepChange(event) {
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    console.log(state.address);
+    listController.addCard(state.address);
 }
 
 function handleInputNumberChange(event) {
